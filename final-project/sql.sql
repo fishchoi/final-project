09426754384767d3f1b7d7c9b38704c01452bbbf
leebj91 <leebj91@naver.com>
2015-06-15 17:37:15 +0900

    회원 테이블 (sql)

---
+++ final-project/sql.sql
@@ -0,0 +1,8 @@
+create table member (
+	memberId varchar2(20) ,
+	memberPass varchar2(20),
+	memberName varchar2(20),
+	memberEmail varchar2(40),
+	memberAddress varchar2(60),
+	primary key (itemId)
+);
